023685457d313a8fe231206f5cfa091c2091d1f5
D070334 <lea-fries@gmx.de>
2019-05-07 10:56:43 +0200

    create base for navigation between menu, game and highscore

--- src/StarExplorer/menu.lua
+++ src/StarExplorer/menu.lua
@@ -7,3 +7,16 @@
 local composer = require( "composer" )
 
 local scene = composer.newScene()
+
+-- -----------------------------------------------------------------------------------
+-- Code outside of the scene event functions below will only be executed ONCE unless
+-- the scene is removed entirely (not recycled) via "composer.removeScene()"
+-- -----------------------------------------------------------------------------------
+
+local function gotoGame()
+	composer.gotoScene( "game" )
+end
+
+local function gotoHighScores()
+	composer.gotoScene( "highscores" )
+end
